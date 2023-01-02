5448143b7c81bd247b75de28fa886ba7e6082c26
thomas-spinec <thomas.spinec@laplateforme.io>
2023-01-02 17:00:44 +0100

    méthodes jusqu'à isConnected faites, début de getAllInfos

--- User.php
+++ User.php
@@ -1,22 +1,15 @@
-<?php 
-    // function connection_bdd() {
-    //     // Connexion à la base de données en local
-    //     $sqli = new mysqli('localhost', 'root', '', 'classes');
-    //     return $sqli;
-    // }
-?>
-
 <?php
+session_start();
 // Création des class
 
 class User {
     /* Propriétés */
-    private int $id;
-    public string $login;
-    private string $password;
-    public string $email;
-    public string $firstname;
-    public string $lastname;
+    private $id;
+    public $login;
+    private $password;
+    public $email;
+    public $firstname;
+    public $lastname;
     private $bdd;
 
     /* Constructeur */
@@ -24,23 +17,31 @@ class User {
     {
         // connection à la BDD
         $this->bdd = new mysqli('localhost', 'root', '', 'classes');
+
+        // Vérification de la connexion
+        if (isset($_SESSION['user'])){
+            $this->id = $_SESSION['user']['id'];
+            $this->login = $_SESSION['user']['login'];
+            $this->password = $_SESSION['user']['password'];
+            $this->email = $_SESSION['user']['email'];
+            $this->firstname = $_SESSION['user']['firstname'];
+            $this->lastname = $_SESSION['user']['lastname'];
+        }
     }
 
     /* Méthodes */
-
+        // Enregistrement
     public function register($login, $password, $email, $firstname, $lastname)
     {
-        // Enregistrement
-        $connect = $this->bdd;
-        $login = mysqli_real_escape_string($connect,htmlspecialchars($login));
-        $password = mysqli_real_escape_string($connect,htmlspecialchars($password));
-        $email = mysqli_real_escape_string($connect,htmlspecialchars($email));
-        $firstname = mysqli_real_escape_string($connect,htmlspecialchars($firstname));
-        $lastname = mysqli_real_escape_string($connect,htmlspecialchars($lastname));
+        $login = mysqli_real_escape_string($this->bdd,htmlspecialchars($login));
+        $password = mysqli_real_escape_string($this->bdd,htmlspecialchars($password));
+        $email = mysqli_real_escape_string($this->bdd,htmlspecialchars($email));
+        $firstname = mysqli_real_escape_string($this->bdd,htmlspecialchars($firstname));
+        $lastname = mysqli_real_escape_string($this->bdd,htmlspecialchars($lastname));
 
         if($login !== "" && $password !== "" && $email !=="" && $firstname !=="" && $lastname !=="" ){
             $requete = "SELECT count(*) FROM utilisateurs where login = '$login'";
-            $exec_requete = $connect -> query($requete);
+            $exec_requete = $this->bdd -> query($requete);
             $reponse      = mysqli_fetch_assoc($exec_requete);
             $count = $reponse['count(*)'];
 
@@ -48,8 +49,8 @@ class User {
 
                 // requête pour ajouter l'utilisateur dans la base de données
                 $password = password_hash($password, PASSWORD_DEFAULT);
-                $requete = "INSERT INTO utilisateurs (login, password, email, firstname, lastname) VALUES ('$login', '$password', '$email', '$firstname', '$lastname')";
-                $exec_requete = $connect -> query($requete);
+                $requete2 = "INSERT INTO utilisateurs (login, password, email, firstname, lastname) VALUES ('$login', '$password', '$email', '$firstname', '$lastname')";
+                $exec_requete2 = $this->bdd -> query($requete2);
                 $error = "Inscription réussie";
                 return $error; // inscription réussie
             }
@@ -63,37 +64,44 @@ class User {
             return $error; // utilisateur ou mot de passe vide
         }
 
-        mysqli_close($connect); // fermer la connexion
+        mysqli_close($this->bdd); // fermer la connexion
     }
 
+        // Connexion
     public function connect($login, $password)
     {
-        // Connexion
-        $connect = $this->bdd;
-        $login = mysqli_real_escape_string($connect,htmlspecialchars($login));
-        $password = mysqli_real_escape_string($connect,htmlspecialchars($password));
+        $login = mysqli_real_escape_string($this->bdd,htmlspecialchars($login));
+        $password = mysqli_real_escape_string($this->bdd,htmlspecialchars($password));
 
         if($login !== "" && $password !== ""){
             $requete = "SELECT count(*) FROM utilisateurs where login = '$login'";
-            $exec_requete = $connect -> query($requete);
+            $exec_requete = $this->bdd -> query($requete);
             $reponse      = mysqli_fetch_assoc($exec_requete);
             $count = $reponse['count(*)'];
 
             if($count!=0){
-                $requete = "SELECT * FROM utilisateurs where login = '$login'";
-                $exec_requete = $connect -> query($requete);
-                $reponse      = mysqli_fetch_assoc($exec_requete);
-                $password_hash = $reponse['password'];
+                $requete2 = "SELECT * FROM utilisateurs where login = '$login'";
+                $exec_requete2 = $this->bdd -> query($requete2);
+                $reponse2      = mysqli_fetch_assoc($exec_requete2);
+                $password_hash = $reponse2['password'];
 
                 if(password_verify($password, $password_hash)){
                     $error = "Connexion réussie";
                     // récupération des données pour les attribuer aux attributs
-                    $this->id = $reponse['id'];
-                    $this->login = $reponse['login'];
-                    $this->password = $reponse['password'];
-                    $this->email = $reponse['email']; 
-                    $this->firstname = $reponse['firstname'];
-                    $this->lastname = $reponse['lastname'];
+                    $this->id = $reponse2['id'];
+                    $this->login = $reponse2['login'];
+                    $this->password = $reponse2['password'];
+                    $this->email = $reponse2['email']; 
+                    $this->firstname = $reponse2['firstname'];
+                    $this->lastname = $reponse2['lastname'];
+                    $_SESSION['user']= [
+                        'id' => $reponse2['id'],
+                        'login' => $reponse2['login'],
+                        'password' => $reponse2['password'],
+                        'email' => $reponse2['email'],
+                        'firstname' => $reponse2['firstname'],
+                        'lastname' => $reponse2['lastname']
+                    ];
                     return $error; // connexion réussie
                 }
                 else{
@@ -111,7 +119,117 @@ class User {
             return $error; // utilisateur ou mot de passe vide
         }
 
-        mysqli_close($connect); // fermer la connexion
+        mysqli_close($this->bdd); // fermer la connexion
+    }
+
+        // Déconnexion
+    public function disconnect()
+    {
+        // verifier la connexion
+        if($this->isConnected()){
+            // rendre les attributs null
+            $this->id = null;
+            $this->login = null;
+            $this->password = null;
+            $this->email = null;
+            $this->firstname = null;
+            $this->lastname = null;
+
+            // détruire la session
+            session_destroy();
+
+            $error = "Déconnexion réussie";
+            return $error; // déconnexion réussie
+        }
+        else{
+            $error = "Vous n'êtes pas connecté";
+            return $error; // vous n'êtes pas connecté
+        }
+    }
+
+        // Suppression
+    public function delete()
+    {
+        //vérification que la personne est connecté
+        if($this->isConnected()){
+            // requête pour supprimer l'utilisateur dans la base de données
+            $requete = "DELETE FROM utilisateurs WHERE id = '$this->id'";
+            $exec_requete = $this->bdd -> query($requete);
+            $this->disconnect();
+            $error = "Suppression et deconnexion réussies";
+            return $error; // suppression réussie
+
+            mysqli_close($this->bdd); // fermer la connexion
+        }
+        else{
+            $error = "Vous n'êtes pas connecté, vous devez être connecté pour supprimer le compte";
+            return $error; // utilisateur non connecté
+        }
+    }
+
+        // Modification
+    public function update($login, $password, $email, $firstname, $lastname)
+    {
+        //vérification que la personne est connecté
+        if($this->isConnected()){
+            //vérification que les champs ne sont pas vides
+            if($login !== "" && $password !== "" && $email !=="" && $firstname !=="" && $lastname !=="" ){
+
+                // récupération des données pour les attribuer aux attributs
+                $this->login = $login;
+                $this->password = $password;
+                $this->email = $email;
+                $this->firstname = $firstname;
+                $this->lastname = $lastname;
+
+                // requête pour modifier l'utilisateur dans la base de données
+                $password = password_hash($password, PASSWORD_DEFAULT);
+                $requete = "UPDATE utilisateurs SET login = '$login', password = '$password', email = '$email', firstname = '$firstname', lastname = '$lastname' WHERE id = '$this->id'";
+                $exec_requete = $this->bdd -> query($requete);
+
+                $error = "Modification réussie";
+                return $error; // modification réussie
+            }
+            else{
+                $error = "Tous les champs ne sont pas renseignés, il faut le login, le mot de passe, l'email, le prénom et le nom";
+                return $error; // utilisateur ou mot de passe vide
+            }
+        }
+        else{
+            $error = "Vous n'êtes pas connecté, vous devez être connecté pour modifier le compte";
+            return $error; // utilisateur non connecté
+        }
+    }
+
+        // Vérification de la connexion
+    public function isConnected()
+    {
+        if($this->id !== null && $this->login !== null && $this->password !== null && $this->email !== null && $this->firstname !== null && $this->lastname !== null){
+            return true; // utilisateur connecté
+        }
+        else{
+            return false; // utilisateur non connecté
+        }
+    }
+
+        // Récupération des données
+    public function getAllInfos()
+    {
+        //vérification que la personne est connecté
+        if($this->isConnected()){
+            // requête pour récupérer les données de l'utilisateur dans la base de données
+            $requete = "SELECT * FROM utilisateurs WHERE id = '$this->id'";
+            $exec_requete = $this->bdd -> query($requete);
+            $reponse = $exec_requete -> fetch_all();
+
+            //affichage
+            var_dump($reponse);
+            foreach ($reponse as $utilisateur)
+            echo  ": ".$utilisateur."<br>";
+        }
+        else{
+            echo "Vous n'êtes pas connecté, vous devez être connecté pour voir les informations du compte";
+        }
     }
 }
 
@@ -137,6 +255,30 @@ class User {
 
         // Test du connect
         // echo $user->connect('test2', 'test2');
+
+        // Test du disconnect
+        // echo $user->disconnect();
+
+        // Test du delete
+        // echo $user->delete();
+
+        // Test du update
+        // echo $user->update('test2bis', 'test2bis', 'test2bis@test.com', 'prenom_test2bis', 'nom_test2_bis');
+
+        // Test du isConnected
+        // echo $user->isConnected();
+
+        // Test du getAllInfos
+        echo $user->getAllInfos();
+
+        echo "<br>";
+        echo $user->login;
+        echo "<br>";
+        echo $user->email;
+        echo "<br>";
+        echo $user->firstname;
+        echo "<br>";
+        echo $user->lastname;
     ?>
 </body>
 </html>
