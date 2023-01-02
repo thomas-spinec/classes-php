11ff981d331170e8201f47a6c9d606e091de5a70
thomas-spinec <thomas.spinec@laplateforme.io>
2023-01-02 14:47:06 +0100

    méthodes register et connect faites

--- User.php
+++ User.php
@@ -20,7 +20,7 @@ class User {
     private $bdd;
 
     /* Constructeur */
-    public function __construct($id, $login, $email, $firstname, $lastname) 
+    public function __construct() 
     {
         // connection à la BDD
         $this->bdd = new mysqli('localhost', 'root', '', 'classes');
@@ -32,23 +32,26 @@ class User {
     {
         // Enregistrement
         $connect = $this->bdd;
-        $login = mysqli_real_escape_string($connect,htmlspecialchars($this->login));
-        $password = mysqli_real_escape_string($connect,htmlspecialchars($this->password));
-        $email = mysqli_real_escape_string($connect,htmlspecialchars($this->email));
-        $firstname = mysqli_real_escape_string($connect,htmlspecialchars($this->firstname));
-        $lastname = mysqli_real_escape_string($connect,htmlspecialchars($this->lastname));
+        $login = mysqli_real_escape_string($connect,htmlspecialchars($login));
+        $password = mysqli_real_escape_string($connect,htmlspecialchars($password));
+        $email = mysqli_real_escape_string($connect,htmlspecialchars($email));
+        $firstname = mysqli_real_escape_string($connect,htmlspecialchars($firstname));
+        $lastname = mysqli_real_escape_string($connect,htmlspecialchars($lastname));
 
-        if($login !== "" && $password !== "" && $email !=="" && $firstname !=="" && $lastname!=="" ){
-            $requete = "SELECT count(*) FROM utilisateurs where login = $login";
+        if($login !== "" && $password !== "" && $email !=="" && $firstname !=="" && $lastname !=="" ){
+            $requete = "SELECT count(*) FROM utilisateurs where login = '$login'";
             $exec_requete = $connect -> query($requete);
-            $reponse      = mysqli_fetch_array($exec_requete);
+            $reponse      = mysqli_fetch_assoc($exec_requete);
             $count = $reponse['count(*)'];
 
             if($count==0){
-                $password = password_hash($password, PASSWORD_DEFAULT);
-                $requete = "INSERT INTO utilisateurs (login, password, email, firstname, lastname) VALUES ($login, $password, $email, $firstname, $lastname)";
-                $exec_requete = $connect -> query($requete);
 
+                // requête pour ajouter l'utilisateur dans la base de données
+                $password = password_hash($password, PASSWORD_DEFAULT);
+                $requete = "INSERT INTO utilisateurs (login, password, email, firstname, lastname) VALUES ('$login', '$password', '$email', '$firstname', '$lastname')";
+                $exec_requete = $connect -> query($requete);
+                $error = "Inscription réussie";
+                return $error; // inscription réussie
             }
             else{
                 $error = "Utilisateur déjà existant";
@@ -56,10 +59,84 @@ class User {
             }
         }
         else{
-            $error = "Tous les champs ne sont pas renseignés, il faut ; // utilisateur ou mot de passe vide
+            $error = "Tous les champs ne sont pas renseignés, il faut le login, le mot de passe, l'email, le prénom et le nom";
+            return $error; // utilisateur ou mot de passe vide
         }
 
-    mysqli_close($connect); // fermer la connexion
+        mysqli_close($connect); // fermer la connexion
     }
 
+    public function connect($login, $password)
+    {
+        // Connexion
+        $connect = $this->bdd;
+        $login = mysqli_real_escape_string($connect,htmlspecialchars($login));
+        $password = mysqli_real_escape_string($connect,htmlspecialchars($password));
+
+        if($login !== "" && $password !== ""){
+            $requete = "SELECT count(*) FROM utilisateurs where login = '$login'";
+            $exec_requete = $connect -> query($requete);
+            $reponse      = mysqli_fetch_assoc($exec_requete);
+            $count = $reponse['count(*)'];
+
+            if($count!=0){
+                $requete = "SELECT * FROM utilisateurs where login = '$login'";
+                $exec_requete = $connect -> query($requete);
+                $reponse      = mysqli_fetch_assoc($exec_requete);
+                $password_hash = $reponse['password'];
+
+                if(password_verify($password, $password_hash)){
+                    $error = "Connexion réussie";
+                    // récupération des données pour les attribuer aux attributs
+                    $this->id = $reponse['id'];
+                    $this->login = $reponse['login'];
+                    $this->password = $reponse['password'];
+                    $this->email = $reponse['email']; 
+                    $this->firstname = $reponse['firstname'];
+                    $this->lastname = $reponse['lastname'];
+                    return $error; // connexion réussie
+                }
+                else{
+                    $error = "Mot de passe incorrect";
+                    return $error; // mot de passe incorrect
+                }
+            }
+            else{
+                $error = "Utilisateur inexistant";
+                return $error; // utilisateur inexistant
+            }
+        }
+        else{
+            $error = "Tous les champs ne sont pas renseignés, il faut le login et le mot de passe";
+            return $error; // utilisateur ou mot de passe vide
+        }
+
+        mysqli_close($connect); // fermer la connexion
+    }
 }
+
+/* *************
+    TEST 
+************** */
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $user = new User();
+
+        // Test du register
+        // echo $user->register('test2', 'test2', 'test2@test.com', 'prenom_test2', 'nom_test2');
+
+        // Test du connect
+        // echo $user->connect('test2', 'test2');
+    ?>
+</body>
+</html>
